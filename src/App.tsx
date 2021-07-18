dfd94bb820be7e5e3264dd19e91876da0db7de9d
Kayo <51717883+jkayoss@users.noreply.github.com>
2021-07-18 19:04:16 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard"
+import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { useState } from 'react';
+import { TransactionsProvider } from "./hooks/useTransactions";
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle/>
+    </TransactionsProvider>
+  );
+}
